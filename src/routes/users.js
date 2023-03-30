a734cf651f8fa02279c65c05dc0c3f144fe47583
pupefrosi <churi_rodriguezf@hotmail.com>
2023-03-29 22:47:55 -0300

    cargo parte de crud users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const routerUsers = express.Router();
 const multer = require('multer');
-const path = ('path');
+const path = require('path');
 const { login, register, saveUser, profile } = require('../controller/usersController');
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname,'./public/img/users'));
+        cb(null, path.join(__dirname,'../../public/img/users'));
     },
     filename: (req, file, cb) => {
         let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-        cb(null, newFilename);
+        cb(null, fileName);
     }
 });
 
 const upload = multer({ storage });
 
+//Formulario de Login
 routerUsers.get('/users/login', login );
 
+//Formulario de Registro
 routerUsers.get('/users/register', register );
+
+//Proceso de Registro o Guardado de Registro
 routerUsers.post('/users/register', upload.single('avatar'), saveUser );
 
+//Perfil de Usuario
 routerUsers.get('/users/profile/:id', profile);
 
 module.exports = routerUsers;
