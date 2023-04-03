cd12084ea4603607b67edd079902f957416d6ac8
pupefrosi <churi_rodriguezf@hotmail.com>
2023-04-03 20:26:09 -0300

    finalizo footer y agrego en todas las vistas todos los parciales

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const methodOverride= require('method-override');
 
 const port = process.env.PORT || 3030
 
-// const publicFolderPath = path.join(__dirname, 'public');
-// console.log(publicFolderPath);
+const publicFolderPath = path.join(__dirname, 'public');
+console.log(publicFolderPath);
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Configuraciones
-//app.use(express.static('public') );
+app.use(express.static('public') );
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({
