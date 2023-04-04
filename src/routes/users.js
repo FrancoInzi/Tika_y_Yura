ead7d2171b6a893228b3d7a882922d832951889d
SantinoRusso <santino.russo04@gmail.com>
2023-04-04 19:02:05 -0300

    CRUD users y modificacion de register

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const routerUsers = express.Router();
 const multer = require('multer');
-const path = ('path');
-const { login, register, saveUser, profile } = require('../controller/usersController');
+const path = require ('path');
+const controller = require('../controller/usersController');
+
+//video 1h
+const { body } = require('express-validator');
+
+const validations = [
+body('fullName').notEmpty().withMessage('Tienes que escribir un nombre'),
+body('email')
+.notEmpty().withMessage('Tienes que escribir un correo electronico').bail()
+.isEmail().withMessage('Debes escribir un formato de correo valido'),
+body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+body('country').notEmpty().withMessage('Tienes que escribir un pais'),
+body('avatar').custom((value, {req }) => {
+    let file = req.file;
+    let acceptedExtensions = ['.jpg', '.png', '.gif'];
+
+    if (!file) {
+        throw new Error('Tienes que subir una imagen');
+    }else{
+        let fileExtension = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(fileExtension)) {
+            throw new Error(`las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+        }
+    }
+    return true;
+})
+]
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -16,11 +42,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-routerUsers.get('/users/login', login );
+routerUsers.get('/users/login',controller.login);
 
-routerUsers.get('/users/register', register );
-routerUsers.post('/users/register', upload.single('avatar'), saveUser );
+routerUsers.get('/users/register', controller.register );
+routerUsers.post('/users/register', upload.single('avatar'), controller.saveUser );
 
-routerUsers.get('/users/profile/:id', profile);
+routerUsers.get('/users/profile/:id', controller.profile);
 
 module.exports = routerUsers;
